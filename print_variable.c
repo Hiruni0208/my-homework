e5fa1fa1b4ba7dac365388b072dcc989796f52a8
Hiruni0208 <90366847+Hiruni0208@users.noreply.github.com>
2021-09-16 10:59:49 +0800

    Add files via upload

---
+++ print_variable.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int b = 87;
+    for (int j = 5; j < 23; j++) {
+            printf("j=%d, b=%d\n", j, b*j);
+        //give me some output messages in following format
+        //and that can be done by add only ONE line code.
+        //please refer to Google slides, page 22.
+        //j=5, b=435
+        //j=6, b=522
+        //j=7, b=609
+        //j=8, b=696
+        //j=9, b=783
+        //...
+        //j=22, b=1914
+    }
+}
