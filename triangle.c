831a6a688010d8e604ee68a235e0b9031e641a80
Hiruni0208 <90366847+Hiruni0208@users.noreply.github.com>
2022-04-19 08:58:50 +0800

    Add files via upload

---
+++ triangle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+main () {
+
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n-i; j++) {
+               if (i % 2) {
+                 printf("+");
+               } else {
+                       printf("*");
+    }
+        }
+        printf("\n");
+    }
+}
